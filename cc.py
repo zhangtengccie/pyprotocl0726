83d09869e2ac102f053123d1b94fa6f1c9535c29
zhangtengccie <zhangtengccie@gmail.com>
2021-07-26 17:51:28 +0800

    Initial commit

---
+++ cc.py
@@ -0,0 +1,7 @@
+import time
+import datetime
+from datetime import datetime,timedelta
+now = datetime.now()
+print(now)
+t1 = now.strftime("%H-%M")
+print(t1)
---
+++ matplotlib_1.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+
+# 设置成中文
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['font.family'] = 'sans-serif'
+colorlist = ['r', 'b', 'g', 'y']
+
+
+def mat_zhu(name_list, size_list):
+    # 调节图形大小，宽，高
+    plt.figure(figsize=(8, 8))
+    # 横向柱状图
+    # plt.barh(name_list, size_list, height=0, color=colorlist)
+    # 竖向柱状图
+    plt.bar(name_list,sorted(size_list),width=0.5, color=colorlist)
+    # 添加主题和注释
+    plt.title('协议与带宽分布')  # 主题
+    plt.xlabel('带宽（M/s）')  # x轴注释
+    plt.ylabel('协议')  # Y轴注释
+    # 保存图片
+    plt.savefig('result1.png')
+    plt.show()
+
+if __name__ == '__main__':
+    size = ['','30','52','10','45']
+    name = ['','http协议','ftp协议','rdp协议','qq协议']
+    mat_zhu(name,size)
